17b759ec4d666b5b7465970004e7ea88819f95ea
DesyTriRhd <102757706+DesyTriRhd@users.noreply.github.com>
2022-04-01 00:59:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,117 @@
-let prevNumber = '';
-let kalkulasiOperator = '';
-let currentNumber = '0';
-const kalkulatorScreen = document.querySelector('.kalkulator-screen');
-const updateScreen = (number) => { /*perbarui layar kalkulator saat tombol angka ditekan */
-    kalkulatorScreen.value = number;
+// Deklarasi Button
+const numbers = document.querySelectorAll(".number")
+const calculatorScreen = document.querySelector('.calculator-screen')
+const operators = document.querySelectorAll(".operator")
+const equalSign = document.querySelector('.equal-sign')
+const clearBtn = document.querySelector('.all-clear')
+const decimal = document.querySelector('.decimal')
+const percentages = document.querySelector('.percentage')
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+// Variable Fungsi
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
 }
 
-const inputNumber = (number) => { /* if statement agar dapat menginput 2 angka lebih */
-    if (currentNumber === '0'){
-        currentNumber = number;
-    } else {
-        currentNumber += number;
-    }
-}
-
-const numbers = document.querySelectorAll(".number");
-numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        inputNumber(event.target.value);
-        updateScreen(currentNumber);
-
-    });
-})
-
-const inputOperator = (operator) => {
-    if (kalkulasiOperator === ''){
-        prevNumber = currentNumber;
-    }
-    kalkulasiOperator = operator;
-    currentNumber = '0';
-}
-
-const operators = document.querySelectorAll(".operator");
-operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value);
-
-    });
-})
-
-const kalkulasi = () => { /*jalankan tombol operasi */
-    let result = '';
-    switch(kalkulasiOperator){
-        case '+':
-        result = parseFloat(prevNumber) + parseFloat(currentNumber)
-        break
-        case '-':
-        result = parseFloat(prevNumber) - parseFloat(currentNumber)
-        break
-        case '*':
-        result = parseFloat(prevNumber) * parseFloat(currentNumber)
-        break
-        case '/':
-        result = parseFloat(prevNumber) / parseFloat(currentNumber)
-        break
+const calculate = () => {
+    let result = ''
+    switch (calculationOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
+            break
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
+            break
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+            break
         default:
             return
     }
     currentNumber = result
-    kalkulasiOperator = ''
+    calculationOperator = ''
 }
 
-const equalSign = document.querySelector('.equal-sign'); /*jalankan tombol sama dengan */
-equalSign.addEventListener("click", () => {
-    console.log('Equal-sign button is pressed')
-    kalkulasi()
-    updateScreen(currentNumber);
-})
-
-const clearAll = () =>{ /*untuk menetapkan angka 0 ke currentNumber */
-    prevNumber = ''
-    kalkulasiOperator = ''
+const inputOperator = (operator) => {
+    if (calculationOperator === '') {
+        prevNumber = currentNumber
+    }
+    calculationOperator = operator
     currentNumber = '0'
 }
 
-const clearBtn = document.querySelector('.all-clear'); /*jalankan tombol AC */
-clearBtn.addEventListener("click", () => {
-    console.log('AC button is pressed')
-    clearAll()
-    updateScreen(currentNumber)
-})
-
-inputDecimal = (dot) => { /* menjalankan tombol . (decimal) */
-     if (currentNumber.includes('.')){ /*agar pemasukkan tanda . (decimal tidak berulang) */
-         return
-     }
-    currentNumber += dot
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    }
+    else {
+        currentNumber += number
+    }
+}
+const updateScreen = (number) => {
+    calculatorScreen.value = number
 }
 
-const decimal = document.querySelector('.decimal') /* menjalankan kalkulasi operasi desimal */
-decimal.addEventListener("click", (Event) => {
-    console.log(Event.target.value)
-    inputDecimal(Event.target.value)
-    updateScreen(currentNumber)
-})
-
-inputPercentage = (percent) => { /* menjalankan tombol percen */
-    if (currentNumber.includes('%')){
+// Fungsi
+inputpercentages = (percent) => {
+    if (currentNumber.includes('%')) {
         return
     }
     currentNumber = currentNumber / 100
 }
 
-const percentages = document.querySelector('.percentage');
-percentages.addEventListener("click", (Event) => {
-    console.log(Event.target.value)
-    inputPercentage(Event.target.value)
+inputDecimal = (dot) => {
+    if (currentNumber.includes('.')) {
+        return
+    }
+    currentNumber += dot
+}
+
+
+numbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+        console.log(number)
+        console.log('number is pressed')
+        console.log(event.target.value)
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        console.log(event.target.value)
+        inputOperator(event.target.value)
+    })
+})
+
+equalSign.addEventListener('click', () => {
+    console.log('equal button is pressed')
+    calculate()
+    updateScreen(currentNumber)
+})
+
+clearBtn.addEventListener('click', () => {
+    console.log('AC button is pressed')
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+decimal.addEventListener('click', (event) => {
+    console.log(event.target.value)
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
+
+percentages.addEventListener('click', (event) => {
+    console.log(event.target.value)
+    inputpercentages(event.target.value)
     updateScreen(currentNumber)
 })
