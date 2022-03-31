11bdb241bed18cf3d5b6bd8b7d41228964954704
DesyTriRhd <102757706+DesyTriRhd@users.noreply.github.com>
2022-04-01 00:55:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const kalkulatorScreen = document.querySelector('.kalkulator-screen');
 const updateScreen = (number) => { /*perbarui layar kalkulator saat tombol angka ditekan */
     kalkulatorScreen.value = number;
 }
+
 const inputNumber = (number) => { /* if statement agar dapat menginput 2 angka lebih */
     if (currentNumber === '0'){
         currentNumber = number;
@@ -12,6 +13,7 @@ const inputNumber = (number) => { /* if statement agar dapat menginput 2 angka l
         currentNumber += number;
     }
 }
+
 const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
@@ -20,6 +22,7 @@ numbers.forEach((number) => {
 
     });
 })
+
 const inputOperator = (operator) => {
     if (kalkulasiOperator === ''){
         prevNumber = currentNumber;
@@ -27,6 +30,7 @@ const inputOperator = (operator) => {
     kalkulasiOperator = operator;
     currentNumber = '0';
 }
+
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
@@ -34,6 +38,7 @@ operators.forEach((operator) => {
 
     });
 })
+
 const kalkulasi = () => { /*jalankan tombol operasi */
     let result = '';
     switch(kalkulasiOperator){
@@ -55,41 +60,48 @@ const kalkulasi = () => { /*jalankan tombol operasi */
     currentNumber = result
     kalkulasiOperator = ''
 }
+
 const equalSign = document.querySelector('.equal-sign'); /*jalankan tombol sama dengan */
 equalSign.addEventListener("click", () => {
     console.log('Equal-sign button is pressed')
     kalkulasi()
     updateScreen(currentNumber);
 })
+
 const clearAll = () =>{ /*untuk menetapkan angka 0 ke currentNumber */
     prevNumber = ''
     kalkulasiOperator = ''
     currentNumber = '0'
 }
+
 const clearBtn = document.querySelector('.all-clear'); /*jalankan tombol AC */
 clearBtn.addEventListener("click", () => {
     console.log('AC button is pressed')
     clearAll()
     updateScreen(currentNumber)
 })
+
 inputDecimal = (dot) => { /* menjalankan tombol . (decimal) */
      if (currentNumber.includes('.')){ /*agar pemasukkan tanda . (decimal tidak berulang) */
          return
      }
     currentNumber += dot
 }
+
 const decimal = document.querySelector('.decimal') /* menjalankan kalkulasi operasi desimal */
 decimal.addEventListener("click", (Event) => {
     console.log(Event.target.value)
     inputDecimal(Event.target.value)
     updateScreen(currentNumber)
 })
+
 inputPercentage = (percent) => { /* menjalankan tombol percen */
     if (currentNumber.includes('%')){
         return
     }
     currentNumber = currentNumber / 100
 }
+
 const percentages = document.querySelector('.percentage');
 percentages.addEventListener("click", (Event) => {
     console.log(Event.target.value)
